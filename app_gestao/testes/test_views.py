0a87be33bb17bac655b93ea9be238c3aa15be8a1
rgomesrgo <sgtbmarafael@hotmail.com>
2025-10-20 20:57:42 -0300

    atualização 20/10

--- app_gestao/testes/test_views.py
+++ app_gestao/testes/test_views.py
@@ -1,5 +1,4 @@
 from django.test import TestCase
-from django.urls import reverse
 from app_gestao.models import CadastroAlunos
 from django.contrib.auth.models import User
 from django.core.files.uploadedfile import SimpleUploadedFile
